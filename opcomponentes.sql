75ab59ffbc21a25749a1f5bb2c7e9a48e198aa8d
SixtoCoca <sixtococa@hotmail.es>
2018-12-13 16:02:32 +0100

    Creacion de la tabla de Promociones y Factura

--- opcomponentes.sql
+++ opcomponentes.sql
@@ -83,6 +83,61 @@ CREATE TABLE `Factura` (
 
 /*MANUEL*/
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `opcomponentes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuario`
+--
+
+CREATE TABLE `Proveedor` (
+  `idProveedor` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `CIF` text NOT NULL,
+  `telefono` text,
+  `direccion` text
+)  DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Usuario`
+--
+ALTER TABLE `Proveedor`
+  ADD PRIMARY KEY (`idProveedor`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Usuario`
+--
+ALTER TABLE `Proveedor`
+  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 /*EUGENIO*/
 
