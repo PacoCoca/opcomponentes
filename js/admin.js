f4c5e966c2aabeff9164c9be1943084b62fd86d7
PacoCoca <pacococacruz@gmail.com>
2019-01-10 13:54:52 +0100

    carga productos db

--- js/admin.js
+++ js/admin.js
@@ -5,3 +5,43 @@ $(document).ready(function(){
 function openContainer(id) {
 	$("#"+id).containerize("open");
 }
+
+function cargaProductos(){
+	$.ajax({
+		url: "php/cargaProductos.php",
+		type: "POST",
+		success: function(respuesta){
+			if(respuesta!="noProductos"){
+				var res = jQuery.parseJSON(respuesta);
+				document.getElementById("bodyTableProducto").innerHTML = "";
+				for (var i=0; i<res.length; i++) {
+					var fila = res[i];
+					var row="<tr>";
+					row+="<td>"+fila[0]+"</td>";
+					row+="<td>"+fila[1]+"</td>";
+					row+="<td>"+fila[2]+"</td>";
+					row+="<td>"+fila[3]+"</td>";
+					row+="<td>"+fila[4]+"</td>";
+					row+="<td>"+fila[5]+"</td>";
+					row+="<td>"+fila[6]+"</td>";
+					row+="<td><button class='btn-warning'><i class='fa fa-minus'></i></button> <button class='btn-success'><i class='fa fa-edit'></i></button></td>";
+					row+="</tr>";
+	
+					document.getElementById("bodyTableProducto").innerHTML += row;
+				}
+			}
+		}
+	});
+}
+
+function cargaPromociones(){
+	
+}
+
+function cargaCatalogo(){
+	
+}
+
+function cargaProveedores(){
+	
+}
---
+++ php/cargaProductos.php
@@ -0,0 +1,25 @@
+<?php  
+
+include 'connection.php';
+
+$query="SELECT * FROM Producto_suple";
+$rs = mysqli_query($conn,$query);
+
+if($rs){
+	$i=0;
+	while($row = mysqli_fetch_array($rs)){
+		$array[$i][0]=$row["nombre"];
+		$array[$i][1]=$row["idProducto"];
+		$array[$i][2]=$row["idProveedor"];
+		$array[$i][3]=$row["coste"];
+		$array[$i][4]=$row["cantidadProducto"];
+		$array[$i][5]=$row["cantidadMin"];
+		$array[$i][6]=$row["cantidadsuple"];
+		$i++;
+	}
+
+	echo json_encode($array);
+} else{
+	echo "noProductos";
+}
+?>
